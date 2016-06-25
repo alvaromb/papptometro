e7a1d69ab3fda324e2ca2c7e7a595809bfb275af
alvaro <me@alvaromb.com>
2016-06-25 12:31:50 +0200

    Android version

--- app/Pactometro/Actions/ResultsActions.js
+++ app/Pactometro/Actions/ResultsActions.js
@@ -32,28 +32,3 @@ export function fetchResults (year?: string = '2016') {
       })
   }
 }
-
-// Old version parsing Politikon website
-// RNFetchBlob.fetch('GET', 'http://politikon.es/elecciones/')
-//   .then(response => {
-//     const html = response.text().replace(/([^;])\n/g, '$1')
-//     // Not very proud of this...
-//     const list = html.match(/Resultados:(.*?)<\/ul>/g)
-//     if (list.length) {
-//       const innerList = list[0]
-//         .replace(/Resultados:<ul>/g, '')
-//         .replace(/<\/ul>/g, '')
-//         .split(/<li>(.*?)<\/li>/g)
-//       const processedList = _.chain(innerList)
-//         .omitBy(v => v === '')
-//         .map(v => ({
-//             partyName: v.substring(0, v.indexOf(':')),
-//             result: v.substring(v.indexOf(':'), v.indexOf(' esc')).slice(1).trim()
-//           })
-//         )
-//         .value()
-//       dispatch(getResults.success(processedList))
-//     } else {
-//       dispatch(getResults.failure())
-//     }
-//   })
